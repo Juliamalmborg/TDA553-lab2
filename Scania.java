5861f26059dff7141bad587c8385e6e66b6aafa6
ViktoriaAndersson <vianders@chalmers.se>
2024-02-01 00:33:50 +0100

    Lite småfixxx

--- Scania.java
+++ Scania.java
@@ -1,10 +1,12 @@
 import java.awt.*;
 public class Scania extends Truck implements TruckPlatform{
-    private boolean truckRamp; //har en ramp
+    private boolean rampIsUp;
+    private double rampAngle;
 
     public Scania(){
         super(2, Color.blue, 200, "Scania");
-        this.truckRamp = true;
+        this.rampIsUp = true;
+        this.rampAngle = 0; //Startvärden
     }
 
     private void incrementAngle(double angle) {
@@ -14,10 +16,20 @@ public class Scania extends Truck implements TruckPlatform{
     private void decrementAngle(double angle) {
         setAngle(getAngle() - angle);
     }
+
+    @Override
+    public boolean isRampUp() {
+        return rampIsUp;
+    }
+    @Override
+    public void setRampState(boolean rampState) {
+
+    }
     @Override
     public void lowerRamp(double angle) { // sänka flaket för att kunna lasta på
         if (getCurrentSpeed() == 0 && angle + getAngle() < 70 && angle >= 0) {
             incrementAngle(angle);
+            setRampState(false);
         } else {
             throw new IllegalArgumentException("Can not have a speed and/or maximum angle is allowed to be 70 degrees.");
         }
@@ -26,10 +38,12 @@ public class Scania extends Truck implements TruckPlatform{
     public void raiseRamp(double angle) { // innebär att lyfta flaket från marken för att sedan kunna köra
         if (getCurrentSpeed() == 0 && getAngle() - angle >= 0 && angle >= 0) {
             decrementAngle(angle);
+            setRampState(true);
         } else {
             throw new IllegalArgumentException("Can not have a speed and/or minimum angle is allowed to be 0 degrees.");
         }
     }
+
     @Override
     protected int getSize(){
         return 3;} //large
--- TruckPlatform.java
+++ TruckPlatform.java
@@ -1,5 +1,6 @@
 public interface TruckPlatform {
+    boolean isRampUp();
     void lowerRamp(double angle);
     void raiseRamp(double angle);
-
+    void setRampState(boolean rampState);
 }
