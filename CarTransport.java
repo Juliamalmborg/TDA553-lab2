e25bef8ec841a2a936232d8c5cee790b2a86ac21
martinkl <martinkl@chalmers.se>
2024-02-02 14:42:14 +0100

    minor clean up

--- CarTransport.java
+++ CarTransport.java
@@ -1,15 +1,7 @@
 import java.awt.*;
-import java.util.Stack;
 import static java.lang.Math.abs;
 
 public class CarTransport extends Truck {
-    /*private boolean rampIsUp; //Rampen är antingen uppe eller nere
-    private Stack<Vehicle> carsOnRamp; //stack som innehåller antal bilar som finns på rampen.
-    private int maxCapacity; //Biltransporten har ett maximalt antal bilar som den kan lasta.
-    private double maxSizeCar; //Bilar som ska lastas på biltransporten får inte vara för stora (eget antagande)
-
-    private int rampAngle; // för att uppdatera rampAngle i Truck??
-    */
 
     private VehicleStorage<Vehicle> storageCapacity;
     private RampWithStates ramp;
@@ -38,31 +30,6 @@ public class CarTransport extends Truck {
             throw new IllegalStateException("No cars to unload or the ramp is not down.");
         }
     }
-
-    /*public void loadCar(Vehicle car) {
-        if (car instanceof CarTransport) {
-            throw new IllegalArgumentException("Cannot load another CarTransport.");
-        }
-        if (!ramp.getIsRampOn() && storageCapacity.getNrCars() <= maxCapacity && distanceCarToTransporter(car)
-                && car.getSize() <= maxSizeCar) {
-            storageCapacity.;
-            car.setXpos(this.getXpos());
-            car.setYpos(this.getYpos());
-        }
-        else {
-            throw new IllegalArgumentException("The car cannot be loaded. Check position of ramp, capacity, car size and distance.");}
-    }
-
-    public Vehicle unloadCar(){
-        if (ramp.getIsRampOn() && storageCapacity.getLoadedCars() > 0) {  //unloadar just nu en och en,
-            Vehicle unloadedcar = storageCapacity.removeLast();
-            unloadedcar.setXpos(this.getXpos()+1); // Vill ha pos när CarTransport,
-            return unloadedcar;
-        }
-        else {
-            throw new IllegalArgumentException("The car cannot be unloaded. Check position of ramp or if ramp empty.");
-        }
-    }*/
 
     public boolean getIsRampOn() {
         return ramp.getIsRampOn();
